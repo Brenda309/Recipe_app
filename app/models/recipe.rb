0a739e4ce7ec900d789f955f95e08a9e2d5f5ecf
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-19 16:14:03 +0200

    Fix linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,5 @@
 class Recipe < ApplicationRecord
-    class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-
-
   validates :name, :preparation_time, :description, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
-validates :name, presence: true
+  validates :name, presence: true
 end
