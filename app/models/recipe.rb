1f1807d96284a5fa4659400660bf386c827f4ebe
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-19 15:34:06 +0200

    Add the vilidation and associtions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+
+
+  validates :name, :preparation_time, :description, presence: true
+
 end
--- app/models/recipefood.rb
+++ app/models/recipefood.rb
@@ -1,2 +1,6 @@
 class Recipefood < ApplicationRecord
+    belongs_to :recipe
+  belongs_to :food
+  validates :quantity, numericality: { only_float: true, greater_than_or_equal_to: 0 }
+
 end
