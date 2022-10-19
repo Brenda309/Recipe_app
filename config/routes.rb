2424897d5934391577fa9b917551e11f2d806d66
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-19 23:41:51 +0200

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   root to: "users#index"
  resources :users
   resources :foods
   resources :recipes do
     resources :recipefoods
+  # Defines the root path route ("/")
   end
 end
