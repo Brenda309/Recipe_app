8c2278716c5928f24ccf168d8dd3993ff9d38339
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-20 15:47:25 +0200

    Add the authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.admin?
-      can :manage, :all
-    else
-      can :read, :all
-    end
+    return unless user.present?
 
+    can :read, :all
+    can :manage, Recipe, user: user
+    can :manage, Food, user: user
+    can :manage, RecipeFood, user: user
+    return unless user.role == 'admin'
+    can :manage, :all
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
