9d8f070ac46724a451c3b47a674cbfc4c405a705
officialesse <uzomaesse@gmail.com>
2022-10-21 12:00:16 +0100

    [feat]: added models for recipe app

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,36 @@
+# Class FoodsController
 class FoodsController < ApplicationController
   def index
-    @foods = current_user.foods
+    @foodlists = Food.all
+  end
+
+  def showshow
+    @foodlist = Food.find(params[:id])
+  end
+
+  def destroy
+    foodlist = Food.find(params[:id])
+    if foodlist.destroy
+      flash[:success] = 'foodList deleted successfully'
+      redirect_to foodlists_path
+    else
+      flash.now[:error] = 'Error: foodList could not be deleted'
+      @foods = Food.all
+    end
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
+    @food.user = current_user
+    if @food.save
+      flash[:success] = 'Ingredient saved successfully'
+      redirect_to foods_path(current_user)
+    else
+      flash.now[:error] = 'Error: Ingredient could not be saved'
+    end
   end
 end
--- app/controllers/recipefoods_controller.rb
+++ app/controllers/recipefoods_controller.rb
@@ -1,5 +1,46 @@
+# Class RecipefoodsController
 class RecipefoodsController < ApplicationController
-  def index
-    @recipe_foods = RecipeFood.find_by(@recipe).includes(:food)
+  def index; end
+
+  def show; end
+
+  def new
+    @recipefood = Recipefood.new
+  end
+
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    @recipefood = Recipefood.new(params.require(:recipefood).permit(:quantity, :food_id))
+    @recipefood.recipe = recipe
+
+    if @recipefood.save
+      flash[:success] = 'Ingredient saved successfully'
+      redirect_to recipe_path(recipe)
+    else
+      flash.now[:error] = 'Error: Ingredient could not be saved'
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipefood = Recipefood.find(params[:recipe_id])
+    recipefood.destroy
+
+    if recipefood.destroy
+      flash[:success] = 'Ingredi'Ingredient'Ingredientent deleted successfully'
+      redirect_to recipe_path(recipe.id)
+    else
+      flash.now[:error] = 'Error: Ingredient could not be deleted'
+    end
+  end
+
+  def edit
+    @recipefood = Recipefood.find(params[:recipe_id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    @recipefood = Recipefood.find(params[:recipe_id]).update(params.require(:recipefood).permit(:quantity, :food_id))
+    redirect_to recipe_path(recipe)
   end
 end
